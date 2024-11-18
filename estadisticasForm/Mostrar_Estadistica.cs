48c9d9b4bbb86e08a977854b1797a5cc1f6ba490
acr301 <acrothschuh@uamv.edu.ni>
2024-11-17 22:01:36 -0600

    graficos de importancia visualmente agregados, con sus nombres para referenciar en el codigo y saber que logica/modelo/datos sinteticos le correspondera

---
+++ estadisticasForm/Mostrar_Estadistica.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estadisticasForm
+{
+    public partial class Mostrar_Estadistica : Form
+    {
+        public Mostrar_Estadistica()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cartesianChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart_flujoPorHora_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+    }
+}
