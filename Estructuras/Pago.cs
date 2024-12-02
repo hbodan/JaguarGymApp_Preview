804c3b9c6e05b7c1515be8acfcedc729b1e7cf4f
Marie <fzogaib@uamv.edu.ni>
2024-12-01 18:40:13 -0600

    Cambios importantes y conflictos sin resolver

--- Estructuras/Pago.cs
+++ Estructuras/Pago.cs
@@ -8,20 +8,18 @@ namespace JaguarGymApp_Preview.Estructuras
 {
     public class Pago
     {
-        public int IdPago { get; set; }
         public string IdTransaccion { get; set; }
         public DateTime FechaRealizacion { get; set; }
-        public string Descripcion { get; set; }
+        public int MesesPagados { get; set; }
         public decimal Monto { get; set; }
         public string Observacion { get; set; }
         public int IdMiembro { get; set; }
 
-        public Pago(int idPago, string idTransaccion, DateTime fechaRealizacion, string descripcion, decimal monto, string observacion, int idMiembro)
+        public Pago(string idTransaccion, DateTime fechaRealizacion, decimal monto, string observacion, int idMiembro, int mesesPagados)
         {
-            IdPago = idPago;
             IdTransaccion = idTransaccion;
             FechaRealizacion = fechaRealizacion;
-            Descripcion = descripcion;
+            MesesPagados = mesesPagados;
             Monto = monto;
             Observacion = observacion;
             IdMiembro = idMiembro;
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace JaguarGymApp_Preview
             //Solo para el commit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Inicio_Sesion());
+            Application.Run(new Ingresar_Pago());
         }
     }
 }
